e248398c833cea6aa9edc031a0f37971578b9848
kbh <wojoo729@naver.com>
2023-04-22 19:45:42 +0900

    0422 commit

--- src/main/java/com/boot/hotel/dto/PaymentDTO.java
+++ src/main/java/com/boot/hotel/dto/PaymentDTO.java
@@ -6,9 +6,9 @@ import lombok.ToString;
 @Data
 @ToString
 public class PaymentDTO {
-	
-    private int pay_num;
-    private int res_num;
+    
+    private Integer pay_num;
+    private Integer res_num;
     private int hotel_id1;
     private String userid1;
     private String username1;
@@ -16,5 +16,7 @@ public class PaymentDTO {
     private int price;
     private int date_num;
     private int people1;
+    
 
+    
 }
--- src/main/java/com/boot/hotel/mapper/PaymentMapper.java
+++ src/main/java/com/boot/hotel/mapper/PaymentMapper.java
@@ -1,7 +1,11 @@
 package com.boot.hotel.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.boot.hotel.dto.HotelDTO;
 import com.boot.hotel.dto.HotelPictureDTO;
 import com.boot.hotel.dto.HotelReservationDTO;
 import com.boot.hotel.dto.PaymentDTO;
@@ -18,11 +22,13 @@ public interface PaymentMapper {
 	public void createReservation(HotelReservationDTO resDto) throws Exception;
 	
 	// 사진 가져오기
-	public String selectImg(HotelPictureDTO picDto) throws Exception;
+	public List<String> searchHotelTitle(Map<String, Object> params) throws Exception;
 	
-	/*
-	public void findUser(PaymentDTO dto) throws Exception;
-	public void findHotelInfo(PaymentDTO dto) throws Exception;
-	public void findHotelPrice(PaymentDTO dto) throws Exception;
-	*/
+	// 호텔 정보 가져오기
+	public List<Map<String, Object>> getHotelInfo(int hotel_id) throws Exception;
+	
+	// 사용자 정보 가져오기
+	public List<Map<String, Object>> getUserInfo(String userid) throws Exception;
+	
+	
 }
