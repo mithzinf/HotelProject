dfd1ac9eadd6a7b31defc694245b78a9a6e78447
ozin <oj.kim514@gmail.com>
2023-04-24 13:26:30 +0900

    1:26 pm commit

--- src/main/java/com/boot/hotel/controller/HotelDetailController.java
+++ src/main/java/com/boot/hotel/controller/HotelDetailController.java
@@ -114,20 +114,23 @@ public class HotelDetailController {
    //테이블 안써도 되고..detail.html부분에서 체크인, 체크아웃 날짜 선택하는 부분에서 그 날짜를 value name으로 받아서 넘기면 될듯?
    //결제페이지에서는 서치밸류 넘기지 말자고 하숏다, roomtype 일일이 치면 된다... 리퀘스트파람을 room1 room2 name 은 type(예약어여서 type1)으로 / value = standard 
    @RequestMapping(value = "/bookRoom", method = RequestMethod.POST)
-   public ModelAndView bookRoom(HttpServletRequest request) throws Exception {
+   public ModelAndView bookRoom(@RequestParam("hotel_id") int hotel_id,@RequestParam("room_type") String room_type,HttpServletRequest request) throws Exception {
 	   		ModelAndView mav = new ModelAndView();
 	   		
 	 
+	   
 	   	 String checkin = "20230426"; // 하드코딩된 checkin 날짜
 	     String checkout = "20230428"; // 하드코딩된 checkout 날짜
 		    
-		  
+	     List<HotelInfoDTO> dto2 = hotelDetailService.getHotelInfoById(hotel_id); //hotel_id 델꼬 올라고 일단 이거 가지고 와밧슴 리스트로..
+	     
+	     	mav.addObject("dto2", dto2);
 		    mav.addObject("checkin", checkin);
 		    mav.addObject("checkout", checkout);
 			System.out.println(checkin);
 			System.out.println(checkout);
-			
-		  
+			System.out.println("호텔아이디 : "+ hotel_id);
+			System.out.println(room_type);
 			mav.setViewName("hotel/bookingTest");   
 
 	   return mav;
