e4b4711311c82b2a35ff69f605505c4a0acd5e0d
e.young <124668531+ggoang@users.noreply.github.com>
2023-04-24 18:29:26 +0900

    20230424(2)

--- src/main/java/com/boot/hotel/service/HotelInfoService.java
+++ src/main/java/com/boot/hotel/service/HotelInfoService.java
@@ -8,8 +8,8 @@ import com.boot.hotel.dto.HotelInfoDTO;
 import com.boot.hotel.dto.HotelPictureDTO;
 
 public interface HotelInfoService {
-	
-    public List<HotelDTO> getHotelList1(Map<String, Object> params) throws Exception;
+
+	public List<HotelDTO> getHotelList1(Map<String, Object> params) throws Exception;
 
     public List<HotelInfoDTO> getHotelList2(Map<String, Object> params) throws Exception;
     
