9baa65e1d25f1b0a4c56940f7357c9bef5e82b89
song <suttan2020@gmail.com>
2023-04-25 13:16:03 +0900

    날짜 검색 적용 준비중 + 지도api 준비 완료

--- src/main/java/com/boot/hotel/controller/HotelMainController.java
+++ src/main/java/com/boot/hotel/controller/HotelMainController.java
@@ -1,5 +1,6 @@
 package com.boot.hotel.controller;
 
+import java.net.URLDecoder;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -12,6 +13,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,10 +22,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.boot.hotel.dto.HotelDTO;
+import com.boot.hotel.dto.HotelInfoDTO;
+import com.boot.hotel.dto.HotelPictureDTO;
+import com.boot.hotel.service.HotelBasketService;
+import com.boot.hotel.service.HotelInfoService;
 import com.boot.hotel.service.HotelMainService;
+import com.boot.hotel.util.MyUtil;
 
 @RestController
 public class HotelMainController {
+	
+	@Autowired
+    private HotelInfoService hotelInfoService;
+ 
+    @Autowired
+    private HotelBasketService hotelBasketService;
+    
+    @Autowired
+    private MyUtil myUtil;
 	
 	@Resource
 	private HotelMainService hotelMainService;
@@ -64,9 +81,9 @@ public class HotelMainController {
 		
 		
 		System.out.println("검색 테스트 컨트롤러 진입");
+		
 		Map<String, Object> paramMap = new HashMap<>();
 		paramMap.put("searchValue", searchValue);
-		//그걸 paramMap에 담음
 		
 		LocalDate check_in_day = LocalDate.parse(check_in, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 		LocalDate check_out_day = LocalDate.parse(check_out, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
@@ -145,6 +162,109 @@ public class HotelMainController {
 		
 		return mav;
 	}
+	
+	
+	@RequestMapping(value = "/hotel/hotelList_test", method = { RequestMethod.GET, RequestMethod.POST })
+    public ModelAndView list(HttpServletRequest request, @RequestParam String check_in, 
+			@RequestParam String check_out)throws Exception {
+		
+		LocalDate check_in_day = LocalDate.parse(check_in, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		LocalDate check_out_day = LocalDate.parse(check_out, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		
+        ModelAndView mav = new ModelAndView();
+
+        String pageNum = request.getParameter("pageNum");
+
+        int currentPage = 1;
+
+        if(pageNum!=null) {
+            currentPage = Integer.parseInt(pageNum);
+        }
+
+        String searchValue = request.getParameter("searchValue");
+
+
+        if (searchValue == null || searchValue.equals("")) {
+            searchValue = "";
+        }
+
+        if (request.getMethod().equalsIgnoreCase("GET") && searchValue != null) {
+            searchValue = URLDecoder.decode(searchValue, "UTF-8");
+        }
+
+        Map<String, Object> params1 = new HashMap<>();
+        params1.put("searchValue", searchValue);
+
+        int dataCount = hotelInfoService.getHotelCount(params1);
+
+        int numPerPage = 10;
+
+        int totalPage = myUtil.getPageCount(numPerPage, dataCount);
+
+        if (currentPage>totalPage) {
+            currentPage = totalPage;
+        }
+
+        int start = (currentPage-1)*numPerPage+1;
+        int end = currentPage*numPerPage;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("start", start);
+        params.put("end", end);
+        params.put("searchValue", searchValue);
+        
+        
+        if(!searchValue.equals("") && searchValue != null) {
+            params.put("searchValue", searchValue);
+        }
+       
+        List<HotelDTO> hotelList1 = hotelInfoService.getHotelList1(params);
+        List<HotelInfoDTO> hotelList2 = hotelInfoService.getHotelList2(params);
+        List<HotelPictureDTO> hotelList3 = hotelInfoService.getHotelList3(params); // 추가된 부분입니다.
+
+        
+        System.out.println("hotelList3 :" + hotelList3);
+        
+        String currentUrl = request.getRequestURL().toString() + "?" + request.getQueryString();
+        String listUrl = currentUrl.replaceAll("&?pageNum=\\d+", "");
+
+       
+
+        String pageIndexList = myUtil.pageIndexList(currentPage, totalPage, listUrl, searchValue);
+
+
+        
+        String detailUrl = "/detail?searchValue=" + 
+                searchValue + "&pageNum=" + currentPage;
+
+        if (searchValue != null && !searchValue.equals("")) {
+            detailUrl += "&searchValue=" + searchValue;
+        }
+
+        if (pageNum != null) {
+            detailUrl += "&" + "pageNum=" + pageNum;
+        }
+
+        
+        mav.addObject("check_in_day",check_in_day);
+        mav.addObject("check_out_day",check_out_day);
+        mav.addObject("hotelList1", hotelList1);
+        mav.addObject("hotelList2", hotelList2);
+        mav.addObject("hotelList3", hotelList3); 
+        mav.addObject("pageIndexList", pageIndexList);
+        mav.addObject("dataCount", dataCount);
+        mav.addObject("currentPage", currentPage);
+        mav.addObject("numPerPage", numPerPage);
+        mav.addObject("detailUrl", detailUrl);
+        mav.addObject("searchValue", searchValue);
+      
+
+        mav.setViewName("hotel/hotellist_test");
+
+        return mav;
+    
+        
+    }
 	
 	
 	
